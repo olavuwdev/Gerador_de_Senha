e99664b551a6ace7dd3de28111e7718dcf5aeda7
Olavo Adriel <ollavoadriel@gmail.com>
2022-12-22 23:23:32 -0300

    commit inicial

---
+++ gerador_de_senha.py
@@ -0,0 +1,11 @@
+import random
+
+low_case = 'ABCDEFGHIJKLMOPQRSTUXYWZ'
+upper_case = low_case.upper()
+number = '1234567890'
+caracters = '*&$#@!*()+{^`'
+size = 8
+
+key = low_case + upper_case + number + caracters
+
+password = key.join
